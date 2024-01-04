09992869706b414bcd8faa7bae830c6cfe6cb699
Nasa19c <nasa19c@gmail.com>
2024-01-04 10:04:47 +0900

    first update in 2024_01_04

---
+++ hello.py
@@ -0,0 +1,25 @@
+import sys
+print(sys.version_info)
+print(sys.version)
+
+print('dkjf',
+                'dkjkl') #일반 들여쓰기보다 4스페이스더
+
+def pri(k: str):
+    print(k)
+
+
+class Book:
+    def __init__(self):
+        self.book
+
+a = b'h\x65llo'
+print(list(a))
+print(a)
+print(type(a))
+# better way 3 byte와 str의 차이를 알아두라
+# ㄴ 이해못함..
+
+# better way 4 f-string formatting에 관련한 얘기로
+# 2024_01_01에 예시 적어놓음
+# 좀더 채워서 정리할 것
