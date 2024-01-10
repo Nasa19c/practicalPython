0509a409d0331846211ec82a50a103ab43c00fe9
Nasa19c <nasa19c@gmail.com>
2024-01-10 22:51:23 +0900

    2024_01_10

---
+++ stdin.py
@@ -0,0 +1,16 @@
+# sys.stdin.readline() 한번 사용해보는 파일
+import sys
+# a = sys.stdin.readline()
+
+# a,b,c = map(int, sys.stdin.readline().split())
+# print(a+b)
+
+# data = list(map(int, sys.stdin.readline().split()))
+# print(data)
+
+a,b,c,d,e,f = map(int, sys.stdin.readline().split())
+for x in range(-999,1000):
+    for y in range(-999,1000):
+        if (a*x)+(b*y) == c and (d*x)+(e*y) ==f:
+            print(x,y)
+            break
